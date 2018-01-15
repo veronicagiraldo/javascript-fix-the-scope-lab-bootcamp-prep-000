7e56101e253795e8b31904e3b4dfaf7fdceb0dce
Veronica Giraldo <veronica9893@gmail.com>
2018-01-15 21:51:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 var animal = 'dog'
 
 function myAnimal() {
-  var animal = 'cat'
   return animal
 }
 
@@ -10,6 +9,7 @@ function yourAnimal() {
   // and the above function both pass?
   // P.S.: You can't just hard-code 'cat' below
   return animal
+  var animal = 'cat'
 }
 
 function add2(n) {
